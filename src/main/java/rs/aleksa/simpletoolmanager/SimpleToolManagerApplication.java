63bbf62cf19e90ae8ab052e040acacd17b75b45a
Aleksa Cakic <aleksa.cakic@gmail.com>
2025-09-09 03:11:55 +0200

    Initial Commit

---
+++ src/main/java/rs/aleksa/simpletoolmanager/SimpleToolManagerApplication.java
@@ -0,0 +1,13 @@
+package rs.aleksa.simpletoolmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleToolManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleToolManagerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/rs/aleksa/simpletoolmanager/SimpleToolManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package rs.aleksa.simpletoolmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleToolManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
